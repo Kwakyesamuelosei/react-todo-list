ad265218fe7f0b3b7860c6a3aac1ab489717eecc
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2020-04-26 18:53:09 +0000

    Initila Commit

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const AboutPage = () => (
+    <div>
+        <h1>About</h1>
+        <p>This app uses React, Redux, React Router, and many other helpful libraries.</p>
+    </div>
+);
+
+export default AboutPage;
